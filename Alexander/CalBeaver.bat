a408a4fa22c9d80a2b8f49a6bcf45aa6e8eea5d5
ElDuderinoSnug <89503474+ElDuderinoSnug@users.noreply.github.com>
2023-11-15 17:36:13 -0500

    our old friend returns

---
+++ Alexander/CalBeaver.bat
@@ -0,0 +1,31 @@
+@echo off
+echo "Howdy, folks! It's Cal Beaver here with some funny messages for ya!"
+pause
+
+echo "Why did the scarecrow win an award?"
+pause
+echo "Because he was outstanding in his field!"
+pause
+
+echo "What do you call a lazy kangaroo?"
+pause
+echo "A pouch potato!"
+pause
+
+echo "What do you call a fish wearing a suit?"
+pause
+echo "Sofishticated!"
+pause
+
+echo "What do you call a bear with no teeth?"
+pause
+echo "A gummy bear!"
+pause
+
+echo "Why did the bicycle fall over?"
+pause
+echo "Because it was two tired!"
+pause
+
+echo "Thanks for listening to my funny messages! Y'all have a good one!"
+pause
