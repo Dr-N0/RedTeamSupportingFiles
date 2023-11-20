cf6313375225e672f880969b7dd7a0ca5f9b7ba2
PHL4R3 <phl4r3.d3v@gmail.com>
2023-11-20 15:36:13 -0500

    Update createusers.bat

--- AD/AD_TESTED/createusers.bat
+++ AD/AD_TESTED/createusers.bat
@@ -1,4 +1,1 @@
-@echo off
-::creates domain admin accounts with names 1-10
-
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command Import-Module ActiveDirectory; $newAdmins = @("username1", "username2", "username3"); Foreach ($user in $newAdmins) { Add-ComputerDomainUser -DomainName "domain.com" -Identity $user -SamAccountName $user -UserAccountControl "1,2,42,541" -Password "Password123!" -GivenName "FirstName" -Surname "LastName" }
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command Import-Module ActiveDirectory; $newAdmins = @("username1", "username2", "username3"); Foreach ($user in $newAdmins) { $securePassword = ConvertTo-SecureString -AsPlainText -String "Password123!" -Force; Add-ComputerDomainUser -DomainName "domain.com" -Identity $user -SamAccountName $user -UserAccountControl "1,2,42,541" -Password $securePassword -GivenName "FirstName" -Surname "LastName" }
